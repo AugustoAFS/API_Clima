6730f36b2ed639a54c9a606c23e9a56ea91c5c2e
AugustoAFS <augusto.farias.santo03@gmail.com>
2024-12-06 11:38:39 -0300

    Atualização nos estilos

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,29 +1,54 @@
 import React, { useState } from 'react';
 import './css/App.css';
+import './css/index.css';
+import './css/card.css';
 import { Canvas } from '@react-three/fiber';
 import Globo from './globo/Globo';
 import Luzes from './globo/Luzes';
-import Card from './components/Card'
+import Card from './components/Card';
+import ImgDay from './assets/icon/lua-crescente.png';
+import ImgNigth from './assets/icon/brilho-do-sol.png';
 import { OrbitControls } from '@react-three/drei';
 
 function App() {
   const [isDay, setIsDay] = useState(true);
 
+  const toggleImage = () => {
+    setIsDay(!isDay);
+  };
+
   return (
     <div className="App">
+      <div className='content'>
+        <h1 className='Titulo'>API CLIMA</h1>
 
-      <h1 className='Titulo'>API CLIMA</h1>
+        <button onClick={toggleImage} className='Button_day_nigth'> 
+          Alternar para {isDay ? 'Noite' : 'Dia'}
+          <img
+            src={isDay ? ImgDay : ImgNigth}
+            alt={isDay ? "Modo Escuro" : "Modo Claro"}
+            className="Button_icon" 
+          />
+        </button>
 
-      <button onClick={() => setIsDay(!isDay)} className='Button_day_nigth'> Alternar para {isDay ? 'Noite' : 'Dia'} </button>
+      </div>
 
+      {/* Card com informações */}
       <Card />
-      <Canvas camera={{ position: [0, 0, 3] }}>
+
+      {/* Canvas do Three.js com Globo */}
+      <Canvas 
+        camera={{    
+          position: [0, 0, 3],
+          fov: 75,
+          near: 0.1,
+          far: 100,
+        }} 
+      >
         <Luzes isDay={isDay} />
         <Globo isDay={isDay} />
         <OrbitControls />
       </Canvas>
-
-
     </div>
   );
 }
